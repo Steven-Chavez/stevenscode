56b6e1b7dbd2f6cf8498ec63c301ecee364613fa
Steven-Chavez <schavez256@yahoo.com>
2014-11-26 11:13:15 -0700

    Re-Structured site, Added social media icons

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Steven Chavez</title>
+		<link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="../css/stylesheet.css">
+	</head>
+	<body>
+		<div class="container">
+			<a href="http://www.stevenscode.com"><h1>Steven Chavez</h1></a>
+			<div class="row mainBody">
+				<div class="nav">
+					<div class="row">
+						<div class="col-md-3 col-sm-3">
+							<h3>Articles</h3>
+						</div>
+						<div class="col-md-3 col-sm-3">
+							<h3>Philosophy</h3>
+						</div>
+						<div class="col-md-3 col-sm-3">
+							<h3>My Art</h3>
+						</div>
+						<div class="col-md-3 col-sm-3">
+							<h3>Contact</h3>
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-4">
+						<img src="https://media.licdn.com/mpr/mpr/shrink_200_200/p/8/005/094/11a/0a98759.jpg" class="img-responsive profileImg" alt="Picture of Steven">
+						<p>
+							<a href="https://www.linkedin.com/pub/steven-chavez/a7/2a3/988">
+								<img class="icons" src="img/icons/LinkedIn-InBug-2CRev(1).png"
+							</a>
+							<a href="https://twitter.com/schavez256">
+								<img class="icons" src="img/icons/Twitter_logo_blue.png"
+							</a>
+							<a href="https://github.com/Steven-Chavez">
+								<img class="icons" src="img/icons/GitHub-Mark-64px.png"
+							</a>
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+
+
+
+	</body>
+</html>
---
+++ php/contact.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title>Contact</title>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="../css/stylesheet.css">
+	</head>
+	<body>
+		<div class="masthead">
+			<div class="container">
+				<nav class="personal-nav">
+					<a class="personal-nav-item active navName" href="http://www.stevenscode.com">
+						Steven Chavez
+					</a>
+					<a class="personal-nav-item" href="#">About Me</a>
+					<a class="personal-nav-item" href="#">Blog</a>
+					<a class="personal-nav-item" href="#">My Work</a>
+					<a class="personal-nav-item" href="#">Resume</a>
+					<a class="personal-nav-item" href="html/contact.php">Contact</a>
+				</nav>
+			</div>
+		</div>
+		<div class="container mainContent">
+			<div class="col-md-6 contact">
+				<article>
+					<h1>Contact</h1>
+					<p>If you wish to contact me please fill the appropriate fields and I will contact you as soon as I can. </p>
+				</article>
+				<p>
+					<label for="name">Name</label>
+					<br>
+					<input id="name" type="text" placeholder="Who may I say is Emailing?">
+				</p>
+				<p>
+					<label for="email">Email</label>
+					<br>
+					<input id="email" type="text"
+				</p>
+
+
+			</div>
+		</div>
+	</body>
+</html>
