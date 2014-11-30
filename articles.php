b12ec50f75507db16c84d05a3ea31c94fdee2f6d
Steven-Chavez <schavez256@yahoo.com>
2014-11-29 21:50:44 -0700

    Added file for each page of my site

--- articles.php
+++ articles.php
@@ -7,7 +7,7 @@
 		<title>Steven Chavez</title>
 		<link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css">
-		<link rel="stylesheet" type="text/css" href="../css/stylesheet.css">
+		<link rel="stylesheet" type="text/css" href="css/stylesheet.css">
 	</head>
 	<body>
 		<div class="container">
@@ -15,7 +15,7 @@
 			<div class="row mainBody">
 				<div class="nav">
 					<div class="row">
-						<div class="col-md-3 col-sm-3 col-xs-6 tab">
+						<div class="col-md-3 col-sm-3 col-xs-6 active">
 							<a href="articles.php">
 								<h3>Articles</h3>
 							</a>
@@ -38,31 +38,8 @@
 					</div>
 				</div>
 				<div class="row">
-					<div class="col-md-3 col-sm-12 col-xs-12">
-						<p>
-							<img src="https://media.licdn.com/mpr/mpr/shrink_200_200/p/8/005/094/11a/0a98759.jpg" class="img-responsive profileImg" alt="Picture of Steven"/>
-						</p>
-						<p>
-							<a href="https://www.linkedin.com/pub/steven-chavez/a7/2a3/988">
-								<img class="icons" src="img/icons/LinkedIn-InBug-2CRev(1).png" /> LinkedIn
-							</a>
-						</p>
-						<p>
-							<a href="https://github.com/Steven-Chavez">
-								<img class="icons" src="img/icons/GitHub-Mark-64px.png" /> /Steven-Chavez
-							</a>
-						</p>
-						<p>
-							<a href="https://twitter.com/schavez256">
-								<img class="icons" src="img/icons/Twitter_logo_blue.png" />
-								@schavez256
-							</a>
-						</p>
-
-					</div>
-					<div class="col-md-9 col-sm-12 col-xs-12 aboutMe">
-						<h2>ABOUT ME</h2>
-						<p>Learning new concepts and acquiring a better understanding of familiar concepts comes easy to me because programming is my passion.</p>
+					<div class="col-md-12 art">
+						<p>The articles written here are for mainly for my personal use so that I can acquire a better understanding of new and familiar concepts. Also It's difficult to find examples that make </p>
 					</div>
 				</div>
 			</div>
--- myArt.php
+++ myArt.php
@@ -7,7 +7,7 @@
 		<title>Steven Chavez</title>
 		<link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css">
-		<link rel="stylesheet" type="text/css" href="../css/stylesheet.css">
+		<link rel="stylesheet" type="text/css" href="css/stylesheet.css">
 	</head>
 	<body>
 		<div class="container">
@@ -16,16 +16,24 @@
 				<div class="nav">
 					<div class="row">
 						<div class="col-md-3 col-sm-3 col-xs-6 tab">
-							<h3>Articles</h3>
+							<a href="articles.php">
+								<h3>Articles</h3>
+							</a>
 						</div>
 						<div class="col-md-3 col-sm-3 col-xs-6 tab">
-							<h3>Resume</h3>
+							<a href="resume.php">
+								<h3>Resume</h3>
+							</a>
 						</div>
 						<div class="col-md-3 col-sm-3 col-xs-6 active">
-							<a href="php/myArt.php"><h3>My Art</h3></a>
+							<a href="myArt.php">
+								<h3>My Art</h3>
+							</a>
 						</div>
 						<div class="col-md-3 col-sm-3 col-xs-6 tab">
-							<h3>Contact</h3>
+							<a href="contact.php">
+								<h3>Contact</h3>
+							</a>
 						</div>
 					</div>
 				</div>
