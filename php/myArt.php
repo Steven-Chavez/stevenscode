1a6d6e94125ce090b3e0678c8b8dc577a965e732
Steven-Chavez <schavez256@yahoo.com>
2014-11-29 21:22:28 -0700

    Updated landing page and other pages to be reactive

--- php/myArt.php
+++ php/myArt.php
@@ -21,7 +21,7 @@
 						<div class="col-md-3 col-sm-3 col-xs-6 tab">
 							<h3>Resume</h3>
 						</div>
-						<div class="col-md-3 col-sm-3 col-xs-6 tab">
+						<div class="col-md-3 col-sm-3 col-xs-6 active">
 							<a href="php/myArt.php"><h3>My Art</h3></a>
 						</div>
 						<div class="col-md-3 col-sm-3 col-xs-6 tab">
@@ -30,34 +30,15 @@
 					</div>
 				</div>
 				<div class="row">
-					<div class="col-md-3 col-sm-12 col-xs-12">
-						<p>
-							<img src="https://media.licdn.com/mpr/mpr/shrink_200_200/p/8/005/094/11a/0a98759.jpg" class="img-responsive profileImg" alt="Picture of Steven"/>
-						</p>
-						<p>
-							<a href="https://www.linkedin.com/pub/steven-chavez/a7/2a3/988">
-								<img class="icons" src="img/icons/LinkedIn-InBug-2CRev(1).png" /> LinkedIn
-							</a>
-						</p>
-						<p>
-							<a href="https://github.com/Steven-Chavez">
-								<img class="icons" src="img/icons/GitHub-Mark-64px.png" /> /Steven-Chavez
-							</a>
-						</p>
-						<p>
-							<a href="https://twitter.com/schavez256">
-								<img class="icons" src="img/icons/Twitter_logo_blue.png" />
-								@schavez256
-							</a>
-						</p>
-
-					</div>
-					<div class="col-md-9 col-sm-12 col-xs-12 aboutMe">
-						<h2>ABOUT ME</h2>
-						<p>Learning new concepts and acquiring a better understanding of familiar concepts comes easy to me because programming is my passion.</p>
+					<div class="col-md-12 art">
+						<h3>Why I Think My Art Background Makes Me a Better Programmer</h3>
+						<p>Art is a craft you can develop over a lifetime and I've been developing it sense I was old enough to pick up a pencil. You can always learn new skills, techniques, or mediums. It's the never ending  aspect of art that attracts me to it and that is the same aspect I find in programming.</p>
 					</div>
 				</div>
 			</div>
 		</div>
+
+
+
 	</body>
 </html>
