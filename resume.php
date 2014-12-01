728b5cdb1dd3afb0d24d35046e9d34a056a394c9
Steven-Chavez <schavez256@yahoo.com>
2014-11-30 21:49:07 -0700

    Started writing resume to site

--- resume.php
+++ resume.php
@@ -37,9 +37,82 @@
 						</div>
 					</div>
 				</div>
-				<div class="row">
-					<div class="col-md-12 resume">
 
+				<div class="row resumePadding">
+					<div class="col-md-12">
+						<h1 class="resumeHeader">Steven Chavez</h1>
+						<p class="resumeContact">schavez256@yahoo.com | (505) 306-3479</p>
+						<p class="resumeContact">
+							<a href="https://www.linkedin.com/pub/steven-chavez/a7/2a3/988">
+								<img class="resumeIcons" src="img/icons/LinkedIn-InBug-2CRev(1).png" /> LinkedIn
+							</a>  |
+							<a href="https://github.com/Steven-Chavez">
+								<img class="resumeIcons" src="img/icons/GitHub-Mark-64px.png" /> /Steven-Chavez
+							</a>
+						</p>
+						<p class="resumeContact">
+							<a href="https://twitter.com/schavez256">
+								<img class="resumeIcons" src="img/icons/Twitter_logo_blue.png" />
+								@schavez256
+							</a>
+						</p>
+						<h2 class="resumeTitles">OBJECTIVE</h2>
+						<p>With sheer determination and hard work I plan on having a successful career in web development. Even once I'm in my field of choice I plan on expanding my knowledge and skills to better myself and the future company I work for.</p>
+						<h2 class="resumeTitles">SKILLS</h2>
+						<ul>
+							<li>
+								Self-motivated, eager to learn new skills and concepts
+							</li>
+							<li>
+								Detail oriented and well organized
+							</li>
+							<li>
+								Logical and resourceful problem solver
+							</li>
+							<li>
+								Database Experience: MySQL and Oracle
+							</li>
+							<li>
+								Object Oriented Programming (PHP, C++)
+							</li>
+							<li>
+								Front-End Programming (HTML, CSS3)
+							</li>
+						</ul>
+						<h2 class="resumeTitles">EDUCATION EXPERIENCE</h2>
+						<p><strong>Central New Mexico Community College</strong></p>
+						<p>
+							<em>
+								Deep Dive Coding Bootcamp (certificate of completion)
+							</em>
+							, October 2014 - Current
+						</p>
+						<ul>
+							<li>
+								Currently attending the Deep Dive Coding Bootcamp at the STEMulus Center where I will be learning HTML, CSS, PHP, JavaScript, JQuery, MySQL and Object-oriented programming with an emphasis on security. Also using GitHub to collaborate, and managing our projects through Asana.
+							</li>
+						</ul>
+						<p><strong>Central New Mexico Community College</strong></p>
+						<p>
+							<em>
+								Computer Information System Concentrated in Programming
+							</em>
+							, January 2014 - Current
+						</p>
+						<ul>
+							<li>
+								Working towards an associates concentrated in programming. Up to date I have the knowledge of C++, object oriented programming, database design (oracle), SQL, and Linux operating system.
+							</li>
+							<li>
+								Knowledge of Linux operating system includes using the openSUSE distribution, and using the terminal to change file permissions, run shell scripts, write shell scripts using vim and manage directories.
+							</li>
+							<li>
+								C++ is where I first learned about multi-file programming, functions, classes, objects, and more concepts like polymorphism.
+							</li>
+							<li>
+								Designed a full data model and deployed a DDL (data description language) with oracle and SQL.
+							</li>
+						</ul>
 					</div>
 				</div>
 			</div>
