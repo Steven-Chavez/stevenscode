53dd092ebc6e297dcbcf44b633456caa6b4d652d
Steven-Chavez <schavez256@yahoo.com>
2015-01-09 20:56:28 -0700

    added articles.js to load articles through ajax

---
+++ js/articles.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Steven on 1/9/2015.
+ */
+$(document).ready(function(){
+
+	$("#loops101").click(function(){
+		$(".articleMain").load("php/articles/loops101.php");
+
+	});
+
+});
